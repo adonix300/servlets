89672a9c9a425e17e8d5b2112c4d10cb9a5c67cf
Danis Abdulgazizov <businessskyjoyboy@gmail.com>
2023-11-23 11:17:09 +0500

    migrated to Spring MVC

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -1,6 +1,8 @@
 package ru.netology.controller;
 
 import com.google.gson.Gson;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 import ru.netology.service.PostService;
@@ -8,9 +10,12 @@ import ru.netology.service.PostService;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+@RestController
+@RequestMapping("/api/posts")
 public class PostController {
   public static final String APPLICATION_JSON = "application/json";
   private final PostService service;
@@ -20,35 +25,22 @@ public class PostController {
     this.service = service;
   }
 
-  public void all(HttpServletResponse response) throws IOException {
-    final var data = service.all();
-    writeResponse(response, data);
+  @GetMapping
+  public List<Post> all() throws IOException {
+    return service.all();
+  }
+@GetMapping("/{id}")
+  public Post getById(@PathVariable long id) throws IOException {
+    return service.getById(id);
   }
 
-  public void getById(long id, HttpServletResponse response) throws IOException {
-    // TODO: deserialize request & serialize response
-    try {
-      final var data = service.getById(id);
-      writeResponse(response, data);
-    } catch (NotFoundException e) {
-      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    }
+  @PostMapping
+  public Post save(@RequestBody Post post) throws IOException {
+    return service.save(post);
   }
-
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    writeResponse(response, data);
-  }
-
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-    try {
-      service.removeById(id);
-      response.setStatus(HttpServletResponse.SC_NO_CONTENT);
-    } catch (NotFoundException e) {
-      response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-    }
+  @DeleteMapping("/{id}")
+  public void removeById(long id) {
+    service.removeById(id);
   }
 
   public void writeResponse(HttpServletResponse response, Object data) throws IOException {
